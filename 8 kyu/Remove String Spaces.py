66b6baeecbdb9d87dbc2f64e83a470259ea4245b
LuisJSosa <94214924+LuisJSosa@users.noreply.github.com>
2022-02-16 16:36:57 -0500

    Update Remove String Spaces.py

--- 8 kyu/Remove String Spaces.py
+++ 8 kyu/Remove String Spaces.py
@@ -1,8 +1,13 @@
-#Simple, remove the spaces from the string, then return the resultant string without using methods.
+#Simple, remove the spaces from the string, then return the resultant string.
 
+#My solution without using methods
 def no_space(x):
     new_string = ""
     for char in x:
         if char != " ":
             new_string += char
     return new_string
+
+#Best answer:
+def no_space(x):
+    return x.replace(' ' ,'')
